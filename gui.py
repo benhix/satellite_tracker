94e5319ed2de7bb1f403898d388d6387ec3ecc87
lordfez <140676908+lordfez@users.noreply.github.com>
2023-12-25 16:41:30 +1030

    Fixed draw on map

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import geocoder
 from PySide6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QLineEdit
-from PySide6.QtGui import QPixmap
+from PySide6.QtGui import QPixmap, QFont
 import satellite_api
 from location import draw_on_map
 
@@ -22,9 +22,11 @@ class MainWindow(QMainWindow):
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
-        # Back label
+        # Background label
         self.background = QLabel(self)
         self.background.setGeometry(0, 0, 1920, 1080)
+        self.bg_img = QPixmap('assets/star_background2.jpg')
+        self.background.setPixmap(self.bg_img)
 
         # Map
         self.world_map = QLabel(self)
@@ -35,6 +37,11 @@ class MainWindow(QMainWindow):
         # Right side text box
         self.right_box = QLabel(self)
         self.right_box.setGeometry(1536, 0, 357, 800)
+        self.right_box.setStyleSheet("color: white;")
+
+        font = QFont()
+        font.setPointSize(15)
+        self.right_box.setFont(font)
         
         # Button
         self.confirm_button = QPushButton('Search', self)
@@ -45,22 +52,30 @@ class MainWindow(QMainWindow):
         self.norad_id = QLineEdit(self)
         self.norad_id.setGeometry(1650, 120, 200, 50)
 
-        # Satellite info label
-        self.satellite_info_label = QLabel("Satellite Data Here")
-
         # Example of updating the label with satellite data
         
 
     def on_click_update_map(self):
         self.iss_id = self.norad_id.text()
         data = satellite_api.get_satellite_position(self.iss_id, observer_lat, observer_long)
-        satname = {data}
-        satlatitude =
-        satlongitude = 
-        satellite_str = f"Name: {data['info']['satname']}
-        for position in data['positions']:
-            satellite_str += f"{position['satlatitude']}, {position['satlongitude']}"
 
-        # Draw satellite on map and update
-        new_map_pixmap = draw_on_map(sat_lat, sat_long)
-        self.world_map.setPixmap(new_map_pixmap)
+        first_position = data['positions'][0] if data['positions'] else None
+        if first_position:
+            sat_lat = first_position['satlatitude']
+            sat_long = first_position['satlongitude']
+
+            # Prepare the satellite information string
+            satellite_str = f"""   Name: {data['info']['satname']}"""
+   
+            for position in data['positions']:
+                satellite_str += f"\n \n   Latitude: {position['satlatitude']}, \n   Longitude: {position['satlongitude']}\n"
+
+            # Update the satellite information label
+            self.right_box.setText(satellite_str)
+
+            # Draw satellite on map and update
+            new_map_pixmap = draw_on_map(sat_lat, sat_long) 
+            self.world_map.setPixmap(new_map_pixmap)
+        else:
+            print("No position data available.")
+
--- location.py
+++ location.py
@@ -1,16 +1,21 @@
 import cv2
+from PySide6.QtGui import QImage, QPixmap
 
 def draw_on_map(lat, long):
     # Load map
     world_map = cv2.imread('assets/map_resize.jpg')
-    height, width = world_map.shape
+    height, width, _ = world_map.shape
 
     # Calc x and y
     x = int((long + 180) * (width / 360))
     y = int((90 - lat) * (height / 180))
 
     # Draw dot on map
-    point_size = 50
+    point_size = 10
     cv2.circle(world_map, (x, y), point_size, (0, 0, 255), -1)
 
-    return world_map
+    # Convert the OpenCV image to QPixmap
+    world_map_qt = QImage(world_map.data, width, height, world_map.strides[0], QImage.Format_BGR888)
+    pixmap = QPixmap.fromImage(world_map_qt)
+
+    return pixmap
